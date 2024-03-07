8fa99707c12449a1e3a5d01902ea339a5142f569
andre <andrey.liskov03@mail.ru>
2024-03-08 00:42:45 +0500

    added simple server config and db connection

--- server/server.js
+++ server/server.js
@@ -1,1 +1,27 @@
-console.log("server working")
+import express from 'express'
+import mongoose from 'mongoose'
+const app = express()
+import Dish from './Dish.js'
+const DB_URL = 'mongodb+srv://andrewrd0364:visagemain0364@cluster0.b5tzfiz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+app.use(express.json())
+
+app.post("/", async (req, res) => {
+    try {
+        const {name, description, picture} = req.body
+        const dish = await Dish.create({name, description, picture})
+        res.json(dish)
+    } catch (e) {
+        res.status(500).json(e)
+    }
+})
+
+async function startApp(){
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(5000, () => {console.log("Server started on port 5000")})
+    } catch (e) {
+        console.log(e)
+    }
+}
+ startApp()
