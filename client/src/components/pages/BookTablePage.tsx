e37478340b214b58926d2d3b72824efc32c6e3e6
Orlov Yakov <orlovaska599@gmail.com>
2024-06-21 13:26:16 +0500

    Добавил сервис для корзины

---
+++ client/src/components/pages/BookTablePage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Header";
+import Cart from "../Cart";
+import Footer from "../Footer";
+
+const BookTablePage: React.FC = () => {
+    return (
+        <>
+            <Header />
+            <Cart />
+            <Footer />
+        </> 
+    );
+};
+
+export default BookTablePage;
