9c0e333d791a6c36c1e6da7a9d6db21d569297be
LrnvAyAnA <nyanya.yara@gmail.com>
2024-06-21 12:23:33 +0500

    Добавила footer, подключила шрифты

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import basket from '../assets/basket.svg';
 import './Header.css';
 
-  const Header: React.FC = () => {
+const Header: React.FC = () => {
 
   return (
     <header className="container">
       <img src={logo} alt="Логотип" />
-      <div style={{ display: 'flex', listStyleType: 'none', width: '40%', justifyContent: 'space-between' }}>
+      <div style={{ display: 'flex', listStyleType: 'none', width: '40%', justifyContent: 'space-between', fontFamily: 'Mukta-Regular', }}>
         <li><a href="/" >Home</a></li>
         <li><a href="/menu" >Menu</a></li>
         <li><a href="/delivery" >Delivery</a></li>
@@ -18,7 +18,7 @@ import './Header.css';
       </div>
       <div style={{ display: 'flex' }}>
         <img src={basket} alt="Корзина" />
-        <button style={{fontWeight: 'bold', fontSize:'16px', }}>Booking now</button>
+        <button style={{ fontFamily: 'Mukta-Bold', fontSize: '16px', }}>Booking now</button>
       </div>
     </header>
   )
--- client/src/components/pages/HomePage.tsx
+++ client/src/components/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Header";
 import Home from "../Home";
 import Menu from "../Menu";
+import Footer from "../Footer";
 
 const HomePage: React.FC = () => {
     return (
@@ -9,6 +10,7 @@ const HomePage: React.FC = () => {
             <Header />
             <Home />
             <Menu />
+            <Footer />
         </>
     );
 };
