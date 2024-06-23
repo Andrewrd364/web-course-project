ad2651cce7696079df3f3f29584c8276d4ae65f7
Orlov Yakov <orlovaska599@gmail.com>
2024-06-24 00:18:32 +0500

    Привязал данные к странице корзины

--- client/src/components/BookTable.tsx
+++ client/src/components/BookTable.tsx
@@ -1,21 +1,59 @@
 import React, { useState } from "react";
-import curve4 from '../assets/curve4.svg'
-import bookTableBG from '../assets/bookTableBG.png';
+import curve4 from "../assets/curve4.svg";
+import bookTableBG from "../assets/bookTableBG.png";
 import TextInput from "./UI/TextInput";
 import RectangularButton from "./UI/RectangularButton";
 import BookTableForm from "./BookTableForm";
 
 const BookTable: React.FC = () => {
     const [checked, setChecked] = useState(false);
-    function handleChange() {
+
+    const handleChange = () => {
         setChecked(!checked);
-    }
+    };
     return (
         <>
-            <div style={{ display: 'flex', justifyContent: 'center', paddingTop: "150px", flexDirection: 'column', alignItems: 'center', marginBottom: '400px' }}>
-                <img style={{ position: 'absolute', right: 0, zIndex: -1, top: "270px" }} src={bookTableBG} alt="" />
-                <img style={{ position: 'absolute', right: 0, zIndex: -100, top: '-550px', width: '100%' }} src={curve4} alt="" />
-                <div style={{ fontSize: '80px', textTransform: 'uppercase', fontFamily: 'Righteous-Regular', marginBottom: "100px" }}>Book a table</div>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    paddingTop: "150px",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    marginBottom: "400px",
+                }}
+            >
+                <img
+                    style={{
+                        position: "absolute",
+                        right: 0,
+                        zIndex: -1,
+                        top: "270px",
+                    }}
+                    src={bookTableBG}
+                    alt=""
+                />
+                <img
+                    style={{
+                        position: "absolute",
+                        right: 0,
+                        zIndex: -100,
+                        top: "-550px",
+                        width: "100%",
+                    }}
+                    src={curve4}
+                    alt=""
+                />
+                <div
+                    style={{
+                        fontSize: "80px",
+                        textTransform: "uppercase",
+                        fontFamily: "Righteous-Regular",
+                        marginBottom: "100px",
+                    }}
+                >
+                    Book a table
+                </div>
                 <BookTableForm />
             </div>
         </>
